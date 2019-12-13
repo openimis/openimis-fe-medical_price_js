2b350e2db948ab1a8a2f11daa447c111c51e07f5
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-12-13 14:24:20 +0100

    locations & hfs: refactorings & code conventions

---
+++ src/pickers/ServicesPriceListPicker.js
@@ -0,0 +1,101 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { injectIntl } from "react-intl";
+
+import {
+    formatMessage,
+    SelectInput
+} from "@openimis/fe-core";
+import { fetchServicesPriceLists } from "../actions"
+
+class ServicesPriceListPicker extends Component {
+    state = {
+        baseOptions: [],
+        regionOptions: [],
+        districtOptions: [],
+    }
+
+    _nullOption = {
+        value: null,
+        label: formatMessage(this.props.intl, "medical_pricelist", this.props.nullLabel || "empty")
+    }
+
+    setOptions = (prevProps) => {
+        if (prevProps.withNull !== this.props.withNull) {
+            this.setState({ baseOptions: [this._nullOption] });
+            return true;
+        }
+        if (!_.isEqual(prevProps.region, this.props.region)) {
+            if (!this.props.region) {
+                this.setState({ regionOptions: [] })
+            } else {
+                this.props.fetchServicesPriceLists(this.props.region);
+            }
+            return true;
+        }
+        if (!_.isEqual(prevProps.district, this.props.district)) {
+            if (!this.props.district) {
+                this.setState({ districtOptions: [] })
+            } else {
+                this.props.fetchServicesPriceLists(this.props.district);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    componentDidMount() {
+        this.setOptions({});
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (!this.setOptions(prevProps)) {
+            if (!_.isEqual(prevProps.fetchedServicesPricelists, this.props.fetchedServicesPricelists)
+                && !!this.props.fetchedServicesPricelists) {
+                if (!!this.props.fetchedServicesPricelists.location.parent) {
+                    this.setState({
+                        districtOptions: this.props.servicesPricelists.map(
+                            o => {return { value: o, label: o.name }}
+                        )
+                    })
+                } else {
+                    this.setState({
+                        regionOptions: this.props.servicesPricelists.map(
+                            o => {return { value: o, label: o.name }}
+                        )
+                    })
+                }
+            }
+        }
+    }
+
+    render() {
+        const { name, value, onChange, readOnly } = this.props;
+        let options = [
+            ...this.state.baseOptions,
+            ...this.state.regionOptions,
+            ...this.state.districtOptions,
+        ];
+        return <SelectInput
+            module="medical_pricelist" label="servicesPricelist"
+            options={options}
+            name={name}
+            value={value}
+            readOnly={readOnly}
+            onChange={onChange}
+        />
+    }
+}
+
+const mapStateToProps = state => ({
+    fetchedServicesPricelists: state.medical_pricelist.fetchedServicesPricelists,
+    servicesPricelists: state.medical_pricelist.servicesPricelists,
+});
+
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ fetchServicesPriceLists }, dispatch);
+};
+
+
+export default injectIntl(connect(mapStateToProps, mapDispatchToProps)(ServicesPriceListPicker));
--- src/reducer.js
+++ src/reducer.js
@@ -11,6 +11,14 @@ function reducer(
         fetchedPricelist: false,
         pricelist: {},
         errorPricelist: null,
+        fetchingServicesPricelists: false,
+        fetchedServicesPricelists: false,
+        servicesPricelists: [],
+        errorServicesPricelists: null,
+        fetchingItemssPricelists: false,
+        fetchedItemsPricelists: false,
+        itemsPricelists: [],
+        errorItemsPricelists: null,
     },
     action,
 ) {
@@ -36,7 +44,7 @@ function reducer(
             return {
                 ...state,
                 fetchingPricelist: false,
-                fetchedPricelist: true,         
+                fetchedPricelist: true,
                 pricelist: {
                     services: arrayToMap(action.payload.data.pricelists.services),
                     items: arrayToMap(action.payload.data.pricelists.items)
@@ -48,6 +56,50 @@ function reducer(
                 ...state,
                 fetchingPricelist: false,
                 errorPricelist: formatServerError(action.payload)
+            };
+        case 'MEDICAL_LOCATION_SERVICES_PRICELIST_REQ':
+            return {
+                ...state,
+                fetchingServicesPricelists: true,
+                fetchedServicesPricelists: false,
+                servicesPricelists: [],
+                errorServicesPricelists: null,
+            };
+        case 'MEDICAL_LOCATION_SERVICES_PRICELIST_RESP':
+            return {
+                ...state,
+                fetchingServicesPricelists: false,
+                fetchedServicesPricelists: action.meta,
+                servicesPricelists: parseData(action.payload.data.servicesPricelists),
+                errorServicesPricelists: formatGraphQLError(action.payload)
+            };
+        case 'MEDICAL_LOCATION_SERVICES_PRICELIST_ERR':
+            return {
+                ...state,
+                fetchingServicesPricelists: false,
+                errorServicesPricelists: formatServerError(action.payload)
+            };
+        case 'MEDICAL_LOCATION_ITEMS_PRICELIST_REQ':
+            return {
+                ...state,
+                fetchingItemsPricelists: true,
+                fetchedItemsPricelists: false,
+                itemsPricelists: [],
+                errorItemsPricelists: null,
+            };
+        case 'MEDICAL_LOCATION_ITEMS_PRICELIST_RESP':
+            return {
+                ...state,
+                fetchingItemsPricelists: false,
+                fetchedItemsPricelists: action.meta,
+                itemsPricelists: parseData(action.payload.data.itemsPricelists),
+                errorItemsPricelists: formatGraphQLError(action.payload)
+            };
+        case 'MEDICAL_LOCATION_ITEMS_PRICELIST_ERR':
+            return {
+                ...state,
+                fetchingItemsPricelists: false,
+                errorItemsPricelists: formatServerError(action.payload)
             };
         default:
             return state;

7723ed29cc11f95e221eba4275d4dea63fd7df0c
Eric Darchis <darchis@pobox.com>
2021-09-06 01:09:12 +0200

    Revert "Migrate pricelists to Django & React"

--- src/actions.js
+++ src/actions.js
@@ -1,231 +1,49 @@
-import {
-  graphql,
-  formatQuery,
-  formatNodeQuery,
-  formatPageQueryWithCount,
-  formatPageQuery,
-  decodeId,
-  graphqlMutation,
-} from "@openimis/fe-core";
-import { ITEMS_PRICELIST_TYPE, SERVICES_PRICELIST_TYPE } from "./constants";
+import { graphql, formatQuery, formatPageQuery, decodeId } from "@openimis/fe-core";
 
 export function fetchPriceLists(servicesPricelist, itemsPricelist) {
-  let filters = [];
-  let projections = [];
-  if (!servicesPricelist && !itemsPricelist) {
-    // nothing to do
-    return () => {};
-  }
-  if (!!servicesPricelist) {
-    filters.push(`servicesPricelistId: ${decodeId(servicesPricelist.id)}`);
-    projections.push("services{id,p}");
-  }
-  if (!!itemsPricelist) {
-    filters.push(`itemsPricelistId: ${decodeId(itemsPricelist.id)}`);
-    projections.push("items{id,p}");
-  }
-  let payload = formatQuery("pricelists", filters, projections);
-  return graphql(payload, "MEDICAL_PRICELIST_LOAD", {
-    servicesPricelist: !!servicesPricelist && servicesPricelist.id,
-    itemsPricelist: !!itemsPricelist && itemsPricelist.id,
-  });
-}
-
-const PRICELIST_BY_ID_PROJECTIONS = [
-  "id",
-  "uuid",
-  "name",
-  "validityFrom",
-  "validityTo",
-  "pricelistDate",
-  "detailsCount:details{totalCount}",
-  "location{id,name,uuid,code,type,parent{id,uuid,code,name,type}}",
-];
-
-export function fetchServicesPricelistById(mm, pricelistId) {
-  const query = formatNodeQuery("ServicesPricelistGQLType", pricelistId, PRICELIST_BY_ID_PROJECTIONS);
-  return graphql(query, "MEDICAL_PRICELIST_PRICELIST", { pricelistType: SERVICES_PRICELIST_TYPE });
-}
-
-export function fetchItemsPricelistById(mm, pricelistId) {
-  const query = formatNodeQuery("ItemsPricelistGQLType", pricelistId, PRICELIST_BY_ID_PROJECTIONS);
-  return graphql(query, "MEDICAL_PRICELIST_PRICELIST", { pricelistType: ITEMS_PRICELIST_TYPE });
+    let filters = []
+    let projections = []
+    if (!servicesPricelist && !itemsPricelist) {
+        // nothing to do
+        return dispatch => { }
+    }
+    if (!!servicesPricelist) {
+        filters.push(`servicesPricelistId: ${decodeId(servicesPricelist.id)}`);
+        projections.push("services{id,p}");
+    }
+    if (!!itemsPricelist) {
+        filters.push(`itemsPricelistId: ${decodeId(itemsPricelist.id)}`);
+        projections.push("items{id,p}");
+    }
+    let payload = formatQuery("pricelists",
+        filters, projections
+    );
+    return graphql(payload, 'MEDICAL_PRICELIST_LOAD', {
+        servicesPricelist: !!servicesPricelist && servicesPricelist.id,
+        itemsPricelist: !!itemsPricelist && itemsPricelist.id
+    });
 }
 
 export function fetchServicesPriceLists(location) {
-  let filters = null;
-  if (!!location) {
-    filters = [`location_Uuid:"${location.uuid}"`];
-  }
-  let projections = ["id", "uuid", "name"];
-  let payload = formatPageQuery("servicesPricelists", filters, projections);
-  return graphql(payload, "MEDICAL_LOCATION_SERVICES_PRICELIST", { location });
+    let filters = null
+    if (!!location) {
+        filters = [`location_Uuid:"${location.uuid}"`]
+    }
+    let projections = ["id", "uuid", "name"]
+    let payload = formatPageQuery("servicesPricelists",
+        filters, projections
+    );
+    return graphql(payload, 'MEDICAL_LOCATION_SERVICES_PRICELIST', { location });
 }
 
 export function fetchItemsPriceLists(location) {
-  let filters = null;
-  if (!!location) {
-    filters = [`location_Uuid:"${location.uuid}"`];
-  }
-  let projections = ["id", "uuid", "name"];
-  let payload = formatPageQuery("itemsPricelists", filters, projections);
-  return graphql(payload, "MEDICAL_LOCATION_ITEMS_PRICELIST", { location });
-}
-
-export function fetchItemsPricelistsSummaries(_, filters = []) {
-  return fetchPricelistsSummaries(_, filters, ITEMS_PRICELIST_TYPE);
-}
-
-export function fetchServicesPricelistsSummaries(_, filters = []) {
-  return fetchPricelistsSummaries(_, filters, SERVICES_PRICELIST_TYPE);
-}
-
-function fetchPricelistsSummaries(_, filters = [], pricelistType) {
-  const projections = [
-    "id",
-    "uuid",
-    "name",
-    "validityFrom",
-    "validityTo",
-    "pricelistDate",
-    "location{id,name,uuid,code,type,parent{id,uuid,code,name,type}}",
-  ];
-  const nodeName = pricelistType === ITEMS_PRICELIST_TYPE ? "itemsPricelists" : "servicesPricelists";
-  const payload = formatPageQueryWithCount(nodeName, filters, projections);
-  return graphql(payload, "MEDICAL_PRICELIST_SUMMARIES", { pricelistType });
-}
-
-export function fetchItemsPricelistDetails(_, filters = [], pricelistId) {
-  const projections = [
-    "id",
-    "uuid",
-    "code",
-    "name",
-    "type",
-    "price",
-    pricelistId && `pricelistDetails(itemsPricelist: "${pricelistId}") {edges {node {priceOverrule}}}`,
-  ].filter(Boolean);
-  const query = formatPageQueryWithCount("medicalItems", filters, projections);
-  return graphql(query, "MEDICAL_PRICELIST_ITEMS");
-}
-
-export function fetchServicesPricelistDetails(_, filters = [], pricelistId) {
-  const projections = [
-    "id",
-    "uuid",
-    "code",
-    "name",
-    "type",
-    "price",
-    pricelistId && `pricelistDetails(servicesPricelist: "${pricelistId}") {edges {node {priceOverrule}}}`,
-  ].filter(Boolean);
-  const query = formatPageQueryWithCount("medicalServices", filters, projections);
-  return graphql(query, "MEDICAL_PRICELIST_SERVICES");
-}
-
-function prepareInput(pricelist) {
-  return {
-    uuid: pricelist.uuid,
-    name: pricelist.name,
-    pricelistDate: pricelist.pricelistDate,
-    locationId: pricelist.location?.uuid,
-    addedDetails: pricelist.addedDetails,
-    removedDetails: pricelist.removedDetails,
-    priceOverrules: pricelist.priceOverrules
-      ? Object.entries(pricelist.priceOverrules).map(([uuid, price]) => ({ uuid, price }))
-      : undefined,
-  };
-}
-
-export function createServicesPricelist(mm, pricelist, clientMutationLabel) {
-  const input = prepareInput(pricelist);
-  input.clientMutationLabel = clientMutationLabel;
-
-  return graphqlMutation(
-    `
-    mutation ($input: CreateServicesPricelistMutationInput!) {
-      createServicesPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
+    let filters = null
+    if (!!location) {
+        filters = [`location_Uuid:"${location.uuid}"`]
     }
-  `,
-    { input }
-  );
-}
-export function createItemsPricelist(mm, pricelist, clientMutationLabel) {
-  const input = prepareInput(pricelist);
-  input.clientMutationLabel = clientMutationLabel;
-
-  return graphqlMutation(
-    `
-    mutation ($input: CreateItemsPricelistMutationInput!) {
-      createItemsPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
-    }
-  `,
-    { input }
-  );
-}
-
-export function updateServicesPricelist(mm, pricelist, clientMutationLabel) {
-  const input = prepareInput(pricelist);
-  input.clientMutationLabel = clientMutationLabel;
-
-  return graphqlMutation(
-    `
-    mutation ($input: UpdateServicesPricelistMutationInput!) {
-      updateServicesPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
-    }
-  `,
-    { input }
-  );
-}
-export function updateItemsPricelist(mm, pricelist, clientMutationLabel) {
-  const input = prepareInput(pricelist);
-  input.clientMutationLabel = clientMutationLabel;
-
-  return graphqlMutation(
-    `
-    mutation ($input: UpdateItemsPricelistMutationInput!) {
-      updateItemsPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
-    }
-  `,
-    { input }
-  );
-}
-
-export function deleteServicesPricelist(mm, uuid, clientMutationLabel) {
-  return graphqlMutation(
-    `
-    mutation ($input: DeleteServicesPricelistMutationInput!) {
-      deleteServicesPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
-    }
-  `,
-    { input: { uuids: [uuid], clientMutationLabel } }
-  );
-}
-export function deleteItemsPricelist(mm, uuid, clientMutationLabel) {
-  return graphqlMutation(
-    `
-    mutation ($input: DeleteItemsPricelistMutationInput!) {
-      deleteItemsPricelist(input: $input) {
-        internalId
-        clientMutationId
-      }
-    }
-  `,
-    { input: { uuids: [uuid], clientMutationLabel } }
-  );
+    let projections = ["id", "uuid", "name"]
+    let payload = formatPageQuery("itemsPricelists",
+        filters, projections
+    );
+    return graphql(payload, 'MEDICAL_LOCATION_ITEMS_PRICELIST', { location });
 }
--- src/components/PriceListLoader.js
+++ src/components/PriceListLoader.js
@@ -5,35 +5,28 @@ import { fetchPriceLists } from "../actions";
 import _ from "lodash";
 
 class PriceListLoader extends Component {
-  componentDidUpdate(prevProps, prevState, snapshot) {
-    if (!_.isEqual(prevProps.fetchingPrice, this.props.fetchingPrice)) {
-      this.props.fetchPriceLists(
-        !this.props.fetchingPrice.servicesPricelist ||
-          !this.props.fetchingPrice.servicesPricelist.id ||
-          !!this.props.servicesPricelists[this.props.fetchingPrice.servicesPricelist.id]
-          ? null
-          : this.props.fetchingPrice.servicesPricelist,
-        !this.props.fetchingPrice.itemsPricelist ||
-          !this.props.fetchingPrice.itemsPricelist.id ||
-          !!this.props.itemsPricelists[this.props.fetchingPrice.itemsPricelist.id]
-          ? null
-          : this.props.fetchingPrice.itemsPricelist
-      );
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (!_.isEqual(prevProps.fetchingPrice, this.props.fetchingPrice)) {
+            this.props.fetchPriceLists(
+                !this.props.fetchingPrice.servicesPricelist || !this.props.fetchingPrice.servicesPricelist.id || !!this.props.servicesPricelists[this.props.fetchingPrice.servicesPricelist.id] ? null : this.props.fetchingPrice.servicesPricelist,
+                !this.props.fetchingPrice.itemsPricelist || !this.props.fetchingPrice.itemsPricelist.id || !!this.props.itemsPricelists[this.props.fetchingPrice.itemsPricelist.id] ? null : this.props.fetchingPrice.itemsPricelist,
+            );
+        }
     }
-  }
-  render() {
-    return null;
-  }
+    render() {
+        return null;
+    }
 }
 
-const mapStateToProps = (state) => ({
-  fetchingPrice: state.medical_pricelist.fetchingPrice,
-  servicesPricelists: state.medical_pricelist.servicesPricelists,
-  itemsPricelists: state.medical_pricelist.itemsPricelists,
+const mapStateToProps = state => ({
+    fetchingPrice: state.medical_pricelist.fetchingPrice,
+    servicesPricelists: state.medical_pricelist.servicesPricelists,
+    itemsPricelists: state.medical_pricelist.itemsPricelists,
 });
 
-const mapDispatchToProps = (dispatch) => {
-  return bindActionCreators({ fetchPriceLists }, dispatch);
+const mapDispatchToProps = dispatch => {
+    return bindActionCreators({ fetchPriceLists }, dispatch);
 };
+
 
 export default connect(mapStateToProps, mapDispatchToProps)(PriceListLoader);

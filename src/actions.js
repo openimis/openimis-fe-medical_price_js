9c231f3763312089e4cae59a47eca83a7cac0f0e
olewandowski <olewandowski@soldevelo.com>
2023-02-01 11:36:26 +0100

    OP-981: Pricelist services/items name validation

--- src/actions.js
+++ src/actions.js
@@ -6,6 +6,7 @@ import {
   formatPageQuery,
   decodeId,
   graphqlMutation,
+  graphqlWithVariables,
 } from "@openimis/fe-core";
 import { ITEMS_PRICELIST_TYPE, SERVICES_PRICELIST_TYPE } from "./constants";
 
@@ -228,4 +229,46 @@ export function deleteItemsPricelist(mm, uuid, clientMutationLabel) {
   `,
     { input: { uuids: [uuid], clientMutationLabel } }
   );
+}
+
+export function medicalServicesValidationCheck(mm, variables) {
+  return graphqlWithVariables(
+    `
+    query ($servicesPricelistName: String!) {
+      isValid: validateServicesPricelistName(servicesPricelistName: $servicesPricelistName)
+    }
+    `,
+    variables,
+    `PRICELIST_SERVICES_VALIDATION_FIELDS`,
+  );
+}
+
+export function medicalServicesValidationClear() {
+  return (dispatch) => {
+    dispatch({ type: `PRICELIST_SERVICES_VALIDATION_FIELDS_CLEAR` });
+  };
+}
+
+export function medicalItemsValidationCheck(mm, variables) {
+  return graphqlWithVariables(
+    `
+    query ($itemsPricelistName: String!) {
+      isValid: validateItemsPricelistName(itemsPricelistName: $itemsPricelistName)
+    }
+    `,
+    variables,
+    `PRICELIST_ITEMS_VALIDATION_FIELDS`,
+  );
+}
+
+export function medicalItemsValidationClear() {
+  return (dispatch) => {
+    dispatch({ type: `PRICELIST_ITEMS_VALIDATION_FIELDS_CLEAR` });
+  };
+}
+
+export function clearMedicalPricelistItems() {
+  return (dispatch) => {
+    dispatch({ type: "MEDICAL_PRICELIST_PRICELIST_CLEAR" });
+  };
 }
--- src/components/PricelistForm.js
+++ src/components/PricelistForm.js
@@ -1,12 +1,22 @@
-import React from "react";
-import { combine, withHistory, withModulesManager, Form } from "@openimis/fe-core";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { withHistory, withModulesManager, Form } from "@openimis/fe-core";
 import PricelistGeneralPanel from "./PricelistGeneralPanel";
 import PricelistDetailsPanel from "./PricelistDetailsPanel";
 import ReplayIcon from "@material-ui/icons/Replay";
+import { clearMedicalPricelistItems } from "../actions";
 
 const PricelistForm = (props) => {
-  const { readOnly, onBack, onSave, onReset, pricelist, onChange, fetchDetails, details } = props;
-  const canSave = () => pricelist.name && pricelist.pricelistDate;
+  const { readOnly, onBack, onSave, onReset, pricelist, onChange, fetchDetails, details, isValid, clearMedicalPricelistItems} = props;
+  const canSave = () => pricelist.name && pricelist.pricelistDate && isValid === true;
+
+  useEffect(() => {
+    return () => { 
+      clearMedicalPricelistItems();
+    }
+  }, []);
+
   return (
     <>
       <Form
@@ -36,5 +46,12 @@ const PricelistForm = (props) => {
   );
 };
 
-const enhance = combine(withHistory, withModulesManager);
-export default enhance(PricelistForm);
+const mapStateToProps = (state) => ({
+  isValid: !!state.medical_pricelist.validationFields?.medicalServices?.isValid || !!state.medical_pricelist.validationFields?.medicalItems?.isValid,
+});
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({ clearMedicalPricelistItems }, dispatch);
+};
+
+export default withHistory(withModulesManager(connect(mapStateToProps, mapDispatchToProps)(PricelistForm)));
